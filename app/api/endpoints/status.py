059a2786221487f28c0c118b021f60b91acdb70d
TeerapongP <thirapongp7@gmail.com>
2025-08-07 17:06:18 +0700

    Enhance read_status endpoint with type hinting and JSON response
    - Added a return type hint to the read_status function for improved clarity and type safety.
    - Imported JSONResponse from FastAPI to facilitate future enhancements for response formatting.

--- app/api/endpoints/status.py
+++ app/api/endpoints/status.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 
 @router.get("/status")
-def read_status():
+def read_status() -> dict:
     return {"status": "UPS System OK"}
